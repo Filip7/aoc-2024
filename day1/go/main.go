1f4864c885e1e6b2367c26694d530bf9b4d74905
Filip M <filipm7@protonmail.com>
2024-12-01 20:23:15 +0100

    day 1 - optimizations
    or syntax sugar 3 - electric boogaloo

--- day1/go/main.go
+++ day1/go/main.go
@@ -32,18 +32,23 @@ func initColumns(col1 *[]int, col2 *[]int, simil *map[int]int) {
 	}
 }
 
-func totalDistance(col1 *[]int, col2 *[]int) int {
-	sum := 0
-	for i := range *col1 {
+func totalDistanceAndSimilarity(col1 *[]int, col2 *[]int, simil *map[int]int) (int, int) {
+	totalDistance, similarity := 0, 0
+	for i, elem := range *col1 {
 		num1 := float64((*col1)[i])
 		num2 := float64((*col2)[i])
 		min := int(math.Min(num1, num2))
 		max := int(math.Max(num1, num2))
 
-		sum += max - min
+		totalDistance += max - min
+
+		val, exists := (*simil)[elem]
+		if exists {
+			similarity += elem * val
+		}
 	}
 
-	return sum
+	return totalDistance, similarity
 }
 
 func similarityScore(col2 *[]int, simil *map[int]int) {
@@ -75,16 +80,9 @@ func main() {
 	sortSlice(&col1)
 	sortSlice(&col2)
 
-	finalSum := totalDistance(&col1, &col2)
-
 	similarityScore(&col2, &simil)
-	var sumSilim uint64 = 0
-	for _, elem := range col1 {
-		val, exists := simil[elem]
-		if exists {
-			sumSilim += uint64(elem * val)
-		}
-	}
+
+	finalSum, sumSilim := totalDistanceAndSimilarity(&col1, &col2, &simil)
 
 	fmt.Println("Final sum: ", finalSum)
 	fmt.Println("Similarity score: ", sumSilim)
