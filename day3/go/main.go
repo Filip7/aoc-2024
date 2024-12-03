8ba63dd52efbcb867bc1c748abdd484bf01827a0
Filip M <filipm7@protonmail.com>
2024-12-03 16:08:36 +0100

    day 3 - reduce code size

--- day3/go/main.go
+++ day3/go/main.go
@@ -10,13 +10,12 @@ import (
 )
 
 // https://regex101.com/ <- life saver
-
 func parseFile() string {
 	var lines []string
 
-	file, err := os.Open("../example2.txt")
+	// file, err := os.Open("../example2.txt")
 	// file, err := os.Open("../example.txt")
-	// file, err := os.Open("../data.txt")
+	file, err := os.Open("../data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,27 +33,15 @@ func parseFile() string {
 	return strings.Join(lines, "")
 }
 
-func handleAllMulInputs(line string) {
-	res := 0
+func handleAllMulInputs(line string) int {
 	r := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	match := r.FindAllString(line, -1)
 
-	match := r.FindAllString(line, len(line))
-
-	for _, str := range match {
-		var num1, num2 int
-
-		fmt.Sscanf(str, "mul(%d,%d)", &num1, &num2)
-
-		res += num1 * num2
-	}
-
-	fmt.Println("All mul: ", res)
+	return caluclateResult(match)
 }
 
 // This works only for example2 and data file
-func handleDoAndDontMulInputs(line string) {
-	res := 0
-
+func handleDoAndDontMulInputs(line string) int {
 	rDoAndDont := regexp.MustCompile(`^(.*?)don't\(\)|do\(\)(.*?)don't\(\)|do\(\)(.*)`)
 	rMul := regexp.MustCompile(`mul\(\d+,\d+\)`)
 
@@ -62,21 +49,27 @@ func handleDoAndDontMulInputs(line string) {
 	allInOneString := strings.Join(matchM, "")
 	match := rMul.FindAllString(allInOneString, -1)
 
+	return caluclateResult(match)
+}
+
+func caluclateResult(match []string) int {
+	res := 0
 	for _, str := range match {
 		var num1, num2 int
-
 		fmt.Sscanf(str, "mul(%d,%d)", &num1, &num2)
 
 		res += num1 * num2
 	}
-
-	fmt.Println("Only do(): ", res)
+	return res
 }
 
 func main() {
 	fmt.Println("AOC 2024 - DAY 3")
 
 	line := parseFile()
-	handleAllMulInputs(line)
-	handleDoAndDontMulInputs(line)
+	resAll := handleAllMulInputs(line)
+	resDoAndDont := handleDoAndDontMulInputs(line)
+
+	fmt.Println("All mul: ", resAll)
+	fmt.Println("Only do(): ", resDoAndDont)
 }
