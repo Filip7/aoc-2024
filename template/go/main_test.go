3f48ffe5b4c6bb6c96aa93a7300257c2af0dbcb2
Filip M <filipm7@protonmail.com>
2024-12-10 22:23:21 +0100

    add test2 in template

--- template/go/main_test.go
+++ template/go/main_test.go
@@ -3,5 +3,19 @@ package main
 import "testing"
 
 func Test(t *testing.T) {
+	res, expected := 0, 100
 	readFile("../example.txt")
+
+	if res != expected {
+		t.Errorf("Result is %d and expected %d!", res, expected)
+	}
+}
+
+func Test2(t *testing.T) {
+	res, expected := 0, 100
+	readFile("../example.txt")
+
+	if res != expected {
+		t.Errorf("Result is %d and expected %d!", res, expected)
+	}
 }
